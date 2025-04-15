410c426ffac70c2172695703ece62954ff9cbedc
terra <terra@bull.bg>
2025-04-15 03:39:47 +0000

    add test actions yml
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml
    
    add test actions yml

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+# image of jenkins
+images = [
+  "https://www.jenkins.io/images/logos/jenkins-x/jenkins-x.png",
+]
+@app.route('/')
+def index():
+  url = random.choice(images)
+  return render_template('index.html', url=url)
+if __name__ =='__main__':
+    app.run(host="0.0.0.0")
